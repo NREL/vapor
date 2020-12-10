d3ede5e54c06d4af4af4c87e1bd1a139a45d20af
Thomas Bowen <tbowen@el1.ib0.cm.hpc.nrel.gov>
2020-12-10 12:53:24 -0700

    added lrmer to maximizing variables

--- vapor/systemsimulator_objects.py
+++ vapor/systemsimulator_objects.py
@@ -169,7 +169,8 @@ class GenericSystemSimulator():
                                 'cambium_portfolio_value',
                                 'cambium_grid_value',
                                 'cambium_co2_rate_avg',
-                                'cambium_co2_rate_marg']:
+                                'cambium_co2_rate_marg',
+                                'cambium_co2_rate_lrmer']:
             return True
         
         elif specific_opt_var in ['lcoe_real','lcoe_nom','marginal_cost_mwh', 'adjusted_installed_cost']:
