af8267e076569447380c59d3711fef344f164770
Thomas Bowen <tbowen@el2.ib0.cm.hpc.nrel.gov>
2020-12-11 11:08:54 -0700

    add lifetime_cambium_co2_rate_lrmer to if_max

--- vapor/systemsimulator_objects.py
+++ vapor/systemsimulator_objects.py
@@ -170,7 +170,8 @@ class GenericSystemSimulator():
                                 'cambium_grid_value',
                                 'cambium_co2_rate_avg',
                                 'cambium_co2_rate_marg',
-                                'cambium_co2_rate_lrmer']:
+                                'cambium_co2_rate_lrmer',
+                                'lifetime_cambium_co2_rate_lrmer']:
             return True
         
         elif specific_opt_var in ['lcoe_real','lcoe_nom','marginal_cost_mwh', 'adjusted_installed_cost']:
