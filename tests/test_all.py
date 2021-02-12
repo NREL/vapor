a3ef15d58b2a6c9dd351883a4a6e2a29c06d9592
Thomas Bowen <thomas.bowen@nrel.gov>
2021-02-12 06:47:58 -0700

    added/uncommented storage test

--- tests/test_all.py
+++ tests/test_all.py
@@ -115,25 +115,24 @@ def test_optimize_lrmer():
 
     simulator.simulate()
 
-
-# def test_storage_unit():
+def test_storage_unit():
     
-#     system_config = {'BatteryTools': {'desired_capacity': 4, 'desired_power': 1000, 'desired_voltage': 500}, 'SystemDesign': {'dc_ac_ratio': 1.2, 'subarray1_azimuth': 180.0, 'subarray1_tilt': 0, 'subarray1_track_mode': 1, 'system_capacity': 100000, 'subarray2_enable': 0, 'subarray3_enable': 0, 'subarray4_enable': 0, 'subarray1_nstrings': 26881, 'inverter_count': 105}, 'Lifetime': {'analysis_period': 25, 'system_use_lifetime_output': 1},
-#                      'PriceSignal': {'mp_enable_energy_market_revenue': 0, 'forecast_price_signal_model': 1, 'mp_enable_ancserv1': 0, 'mp_enable_ancserv2': 0, 'mp_enable_ancserv3': 0, 'mp_enable_ancserv4': 0}, 'BatterySystem': {'en_batt': 1, 'batt_meter_position': 1}, 'BatteryDispatch': {'batt_dispatch_auto_can_charge': 1, 'batt_dispatch_auto_can_clipcharge': 1, 'batt_dispatch_auto_can_gridcharge': 1, 'batt_dispatch_choice': 1}}
-#     resource_file = 'data/PySAM Downloaded Weather Files/-89.578_39.394_psm3_60_tmy.csv'
+    system_config = {'BatteryTools': {'desired_capacity': 4, 'desired_power': 1000, 'desired_voltage': 500}, 'SystemDesign': {'dc_ac_ratio': 1.2, 'subarray1_azimuth': 180.0, 'subarray1_tilt': 0, 'subarray1_track_mode': 1, 'system_capacity': 100000, 'subarray2_enable': 0, 'subarray3_enable': 0, 'subarray4_enable': 0, 'subarray1_nstrings': 26881, 'inverter_count': 105}, 'Lifetime': {'analysis_period': 25, 'system_use_lifetime_output': 1},
+                     'PriceSignal': {'mp_enable_energy_market_revenue': 0, 'forecast_price_signal_model': 1, 'mp_enable_ancserv1': 0, 'mp_enable_ancserv2': 0, 'mp_enable_ancserv3': 0, 'mp_enable_ancserv4': 0}, 'BatterySystem': {'en_batt': 1, 'batt_meter_position': 1}, 'BatteryDispatch': {'batt_dispatch_auto_can_charge': 1, 'batt_dispatch_auto_can_clipcharge': 1, 'batt_dispatch_auto_can_gridcharge': 1, 'batt_dispatch_choice': 1}}
+    resource_file = 'data/PySAM Downloaded Weather Files/-89.578_39.394_psm3_60_tmy.csv'
 
-#     cambium_df = datafetcher.load_cambium_data(aggregate_region='inter', scenario='StdScen20_MidCase')
-#     cambium = Cambium(cambium_df, 'inter', 'western', resource_file)
-#     cambium.clean()
+    cambium_df = datafetcher.load_cambium_data(aggregate_region='inter', scenario='StdScen20_MidCase')
+    cambium = Cambium(cambium_df, 'inter', 'western', resource_file)
+    cambium.clean()
 
-#     model = PVMerchantPlant(system_config, resource_file, cambium, 'western')
+    model = PVMerchantPlant(system_config, resource_file, cambium, 'western')
 
-#     # from models 'execute_all()' for Generic Plant Class
-#     model.run_generator()
-#     if model.load == None:
-#         model.size_load()
-#     model.make_gen_profile_no_batt() #8760 * analysis period of generation without battery
-#     if model.storage: 
-#         model.run_battery()
-#     print(model.storage)
-#     # print(f"PV no storage, irr {model.outputs['project_return_aftertax_irr']}, npv {model.outputs['project_return_aftertax_npv']}")
+    # from models 'execute_all()' for Generic Plant Class
+    model.run_generator()
+    if model.load == None:
+        model.size_load()
+    model.make_gen_profile_no_batt() #8760 * analysis period of generation without battery
+    if model.storage: 
+        model.run_battery()
+    print(model.storage)
+    # print(f"PV no storage, irr {model.outputs['project_return_aftertax_irr']}, npv {model.outputs['project_return_aftertax_npv']}")
